8ba1f14b02d6edd3be70db0006afdc438eed043b
guilhermexavyer <guilhermexavierpessoal@gmail.com>
2024-10-20 19:49:34 -0300

    Visualização da senha

---
+++ assets/js/ocultar_senha.js
@@ -0,0 +1,10 @@
+const togglePassword = document.querySelector('.ocultar-senha');
+const passwordInput = document.getElementById('senha');
+
+togglePassword.addEventListener('click', function () {
+    const type = passwordInput.getAttribute('type') === 'password' ? 'text' : 'password';
+    passwordInput.setAttribute('type', type);
+
+    this.classList.toggle('fa-eye');
+    this.classList.toggle('fa-eye-slash');
+});
